f5ce988318a464434d81a1f583bbc238e953fc60
Dell <qiushi.han@my.jcu.edu.au>
2022-08-06 18:28:04 +0800

    Making the programme display data nicely

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    parts_info = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +24,13 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        parts_info.append(parts)
     input_file.close()
+    return parts_info
 
+def display_data(data):
+    """Display the data in a clear format"""
+    for info in data:
+        print("{} is taught by {:12} and has {:4} students".format(*info))
 
 main()
