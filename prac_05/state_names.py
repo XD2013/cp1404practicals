ad0981eae48f58aabd19fe7871e23d1aaac63f80
Dell <qiushi.han@my.jcu.edu.au>
2022-08-18 00:38:23 +0800

    Modified the format and added upper()

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,13 +6,15 @@ GitHub: https://github.com/XD2013/cp1404practicals
 """
 
 # Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {'QLD': "Queensland", 'NSW': "New South Wales", 'NT': "Northern Territory", 'WA': "Western Australia",
+                'ACT': "Australian Capital Territory", 'VIC': "Victoria", 'TAS': "Tasmania"}
+STATE_NAME = []
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ") .upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ") .upper()
