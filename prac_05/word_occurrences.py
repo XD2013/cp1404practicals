c4d7dd87ed5d786ffdcb47a76e2d85b7f959e82e
Dell <qiushi.han@my.jcu.edu.au>
2022-08-26 21:15:15 +0800

    Modified the last print statements in word_occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -11,11 +11,11 @@ words = text.split()
 for word in words:
     frequency = counted_word.get(word, 0)
     counted_word[word] = frequency + 1
+print(counted_word)
 words = list(counted_word.keys())
-print(words)
 words.sort()
-print(words)
-length = max((len(word) for word in words))
-print(length)
+# length = max((len(word) for word in words))
+# for word in words:
+#     print("{:{}}  :  {}".format(word, length, counted_word[word]))
 for word in words:
-    print("{:{}} : {}".format(word, length, counted_word[word]))
+    print(f"{word:<15}:  {counted_word[word]}")
