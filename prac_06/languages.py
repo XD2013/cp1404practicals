fae5742d3c7d8d54b4b82fdd44435bf5d68f50f9
Dell <qiushi.han@my.jcu.edu.au>
2022-08-27 23:13:08 +0800

    Finished the parts up to printing the python object

---
+++ prac_06/languages.py
@@ -0,0 +1,8 @@
+"""CP1404/CP5632 Practical - Languages"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+print(python)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
