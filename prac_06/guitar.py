db2d156b05c62b9e0f603c37b2812417d14de1c6
Dell <qiushi.han@my.jcu.edu.au>
2022-08-28 16:32:18 +0800

    Initial commit of guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,24 @@
+"""CP1404/CP5632 Practical - Guitar calss"""
+
+YEAR = 2022
+AGE = 50
+
+class Guitar:
+    """Represent a Guitar object."""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a formatted information of a Guitar."""
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """Get the age of a guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if a Guitar is considered vintage."""
+        return self.get_age() >= VINTAGE_AGE
