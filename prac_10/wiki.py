4e6246c9907c882e2c2cf53130a2d8414fdee28c
Dell <qiushi.han@my.jcu.edu.au>
2022-09-28 19:54:32 +0800

    Modify some details in wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,10 +1,9 @@
 import wikipedia
 
-def main():
-    search_thing = input("Pleasr input the page title or search phrase: ")
-    while search_thing != "":
-        content = wikipedia.summary(search_thing)
-        print(content)
-        search_thing = input("Pleasr input the page title or search phrase: ")
 
-main()
+search = input("Input your search string: ")
+while search != "":
+    content = wikipedia.summary(search)
+    print(content)
+    search = input("Input your search string: ")
+
